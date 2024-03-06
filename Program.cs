ef17f71b3e723b40fabcf0b5dc04c1f0a38ae146
Maria <mariya-ivanova@list.ru>
2024-03-06 21:32:20 +0200

    the solution was just added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Insert a number for week day: ");
+int N = int.Parse(Console.ReadLine());
+if (N == 6 || N == 7)
+ {
+   Console.WriteLine($"{N} -> yes");
+ }
+ else
+  {
+   Console.WriteLine($"{N} -> no");
+  }
